1028489a28ac53529ca9f1e2fc765d2a10bebc52
Vineet Bhoriya <105233380+vineetbhoriya@users.noreply.github.com>
2023-06-12 10:07:13 +0530

    home page added

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import Navbar from "../features/navbar/Navbar"
+import Product from "../features/product-list/ProductList"
+export default function Home() {
+    return (
+        <Navbar>
+            <Product></Product>
+        </Navbar>
+    )
+}
